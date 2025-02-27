a67a1aa96247d9b766faeb5ffeaa61177150af34
ManNjoro <elijohnmwoho@gmail.com>
2025-02-28 02:03:04 +0300

    Data validation

--- store/views.py
+++ store/views.py
@@ -7,14 +7,26 @@ from .serializers import ProductSerializer
 
 # Create your views here.
 
-@api_view()
+
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
-    
+    if request.method == 'GET':
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(queryset, many=True, context={"request": request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        return Response('OK')
+
 @api_view()
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response("ok")
