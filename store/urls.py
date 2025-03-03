b9f1be7d9f435fecb30e18a8b633b2fbd4abd3a2
ManNjoro <elijohnmwoho@gmail.com>
2025-03-03 17:10:51 +0300

    Refactor views to use Django REST Framework's built-in view classes

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('product/', views.product_list),
-    path('product/<int:id>/', views.product_detail),
-    path('product/<int:id>/', views.product_detail),
+    path('product/', views.ProductList.as_view()),
+    path('product/<int:id>/', views.ProductDetail.as_view()),
     path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
     path('collections/', views.collection_list),
 ]
