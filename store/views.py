4474c1f14a79e5a305086ae6f7d91b47f822f690
ManNjoro <elijohnmwoho@gmail.com>
2025-03-04 11:29:06 +0300

    Added basename to ProductViewSet and implemented filtering by collection_id in ProductViewSet

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models.aggregates import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,8 +14,14 @@ from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializ
 # Create your views here.
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get('collection_id')
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset
 
     def get_serializer_context(self):
         return {"request": self.request}
