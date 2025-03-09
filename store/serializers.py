279094a2086a0747fdf457b205e01eebd983a2cf
ManNjoro <elijohnmwoho@gmail.com>
2025-03-09 17:14:28 +0300

    Refactor signal imports and handling in core and store apps

--- store/serializers.py
+++ store/serializers.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
 
+from .signals import order_created
 from .models import (
     Cart,
     CartItem,
@@ -182,5 +183,7 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
 
             Cart.objects.filter(pk=cart_id).delete()
+            
+            order_created.send_robust(self.__class__, order=order)
             
             return order
